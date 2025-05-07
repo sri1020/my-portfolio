fdb8f311f0fc5b87b2fff643a31bf9d8a6b84acf
Srilekha Mukkala <mukkala2705@gmail.com>
2025-05-07 17:22:49 -0500

    Update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,8 +10,10 @@ const Hero = () => {
       <h3>Full Stack Developer | Web Enthusiast</h3>
       <p>I build modern and responsive web applications.</p>
       <a href="/resume.pdf" download="Resume_Srilekha_Mukkala">
-        <button className="resume-btn">Download Resume</button>
-      </a>
+  <button className="resume-btn" title="Click to download my resume">
+    Download Resume
+  </button>
+</a>
     </section>
   );
 };
