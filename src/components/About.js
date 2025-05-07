6153b933c7c74d1a0b6e3392e940e7650ecd4b72
Srilekha Mukkala <mukkala2705@gmail.com>
2025-05-07 17:36:36 -0500

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section className="about" id="about">
       <h2>About Me</h2>
-      <p>I am a software engineer with experience in Java, React, and cloud technologies. I love solving problems and building scalable web apps.</p>
+      <p>With over 5 years of experience as a Java developer, I have built scalable, maintainable back-end systems and designed and integrated RESTful APIs using Spring Boot. I have a strong understanding of SQL for database interactions and am skilled in automation scripting for deployment and production support tasks. I have hands-on experience using Splunk and Dynatrace for monitoring and troubleshooting. I ensure code quality through JUnit and Mockito for testing and use SonarCloud to maintain high standards. My work follows Agile methodologies, contributing to continuous integration and full SDLC processes.</p>
     </section>
   );
 };
